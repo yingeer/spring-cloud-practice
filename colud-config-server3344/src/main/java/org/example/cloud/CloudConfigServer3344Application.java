dc62eefe120920ed57c83b283f246710af9ced86
yingeer <2973780290@qq.com>
2020-03-13 20:53:17 +0800

    config client 配置完

---
+++ colud-config-server3344/src/main/java/org/example/cloud/CloudConfigServer3344Application.java
@@ -0,0 +1,13 @@
+package org.example.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigServer3344Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigServer3344Application.class);
+    }
+}
